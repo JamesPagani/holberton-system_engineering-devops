47880c5b2227dc4e1514c8d06d3c0525e9a0f201
JamesPagani <jaimeagalvezv@gmail.com>
2020-03-24 00:14:18 -0500

    Made sure that Puppet uses shell to run the command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Kills a process called "killmenow"
 exec {'pkill':
-  command => 'pkill killmenow',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  command  => 'pkill killmenow',
+  path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  provider => 'shell'
 }
