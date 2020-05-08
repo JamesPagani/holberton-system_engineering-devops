184e0d645cbd9c9e74d53d17b49d31df75c4308e
= <=>
2020-05-08 01:26:42 +0000

    Changed user-agent header and made sure to retrieve the data before requesting the amount of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the amount of subscribers of a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'user-agent': 'web:0x16-0.how_many_subs:v0.1.0'}
+    headers = {'user-agent': 'linux:how_many_subs:v0.1.0'}
 
     subreddit = requests.get(url, allow_redirects=False,
                              headers=headers).json()
-    subs = subreddit.get("subscribers", 0)
+    subs = subreddit.get("data", 0).get("subscribers", 0)
 
     return subs
