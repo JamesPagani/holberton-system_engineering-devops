6c4debba4264294744a02716f4a07f66d5ca2f4f
= <=>
2020-05-08 01:48:46 +0000

    Checking for invalid subreddits

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,15 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the amount of subscribers of a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'user-agent': 'linux:how_many_subs:v0.1.0'}
+    headers = {'user-agent': 'windows:subs:v1.1.0'}
 
     subreddit = requests.get(url, allow_redirects=False,
-                             headers=headers).json()
-    subs = subreddit.get("data", 0).get("subscribers", 0)
+                             headers=headers)
+
+    if subreddit.status_code >= 300:
+        return 0
+
+    sub_r_data = subreddit.json()
+    subs = sub_r_data.get("data", 0).get("subscribers", 0)
 
     return subs
