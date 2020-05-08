a84360d805265c86700142cd668dbf9a7e5ac90c
= <=>
2020-05-08 02:58:57 +0000

    Top ten posts of a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Top ten
+Make a request to Reddit and find the top 10 hot posts in a subreddit.
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """Return the amount of subscribers of a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'user-agent': 'windows:subs:v1.1.0'}
+
+    subreddit = requests.get(url, allow_redirects=False,
+                             headers=headers)
+
+    if subreddit.status_code != 200:
+        return None
+
+    sub_json = subreddit.json()
+    hot_posts = sub_json.get("data", None).get("children", None)
+
+    for post in hot_posts:
+        post_title = post.get("data", None).get("title", None)
+        print(post_title)
